a35e1963f0df300c21a6a5ad54707ed2f3ed9544
unbounddev <unbounddev@git>
2025-10-14 14:33:05 -0700

    Refactor custom display name for vehicles

--- addons/vehicles/models/vehicles_vehicle.py
+++ addons/vehicles/models/vehicles_vehicle.py
@@ -15,5 +15,4 @@ class VehiclesVehicle(models.Model):
     @api.depends('make', 'model', 'license', 'vin')
     def _compute_display_name(self):
         for record in self:
-            record.display_name = f"{record.make} {record.model} ({record.license}) [{record.vin}]"
             record.display_name = '%s%s%s%s' % (record.make and '%s ' % record.make or '', record.model and '%s ' % record.model or '', record.license and '%s ' % f"({record.license})" or '', record.vin and '%s' % f"[{record.vin}]" or '')
